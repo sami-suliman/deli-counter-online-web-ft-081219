2fb0ce398f49565c1710cf5f5994b8b961ff7ac4
sami suliman <sami.bco@gmail.com>
2019-08-29 01:57:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+require "pry"
+def line(katz_deli) 
+ 
+if katz_deli != []
+ num_list =  katz_deli.map.with_index do |name, index|
+    "#{index+1}. #{name}"
+  end 
+  
+  puts "The line is currently: #{num_list.join(" ")}"
+else 
+  puts "The line is currently empty."
+end 
+end 
